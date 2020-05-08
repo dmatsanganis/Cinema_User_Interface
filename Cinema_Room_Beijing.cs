1885b53633c30b38ab6b5b84a69600e42d0bd233
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-05-08 21:46:26 +0300

    Add Cinema_Room_Beijing.cs

--- Cinema_Room_Beijing.cs
+++ Cinema_Room_Beijing.cs
@@ -1,1 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cinema
+{
+    public partial class Cinema_Room_Beijing : Form
+    {
+        public Cinema_Room_Beijing()
+        {
+            InitializeComponent();
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            var form1 = new Cinema_Rooms_Hall();
+            form1.Closed += (s, args) => this.Close();
+            form1.Show();
+            this.Hide();
+        }
+    }
+}
 
