d18d1a075b5d66c90bb1cf5fae8bc3dca4623064
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-05-12 00:07:10 +0300

    Add Cinema_Room_YaoHall.cs

--- Cinema_Room_YaoHall.cs
+++ Cinema_Room_YaoHall.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
+namespace Cinema
+{
+    public partial class Cinema_Room_YaoHall : Form
+    {
+        public Cinema_Room_YaoHall()
+        {
+            InitializeComponent();
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            var form1 = new Cinema_Rooms_Hall();
+            form1.Closed += (s, args) => this.Close();
+            form1.Show();
+            this.Hide();
+        }
+    }
+}
