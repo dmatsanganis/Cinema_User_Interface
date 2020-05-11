77196f4021a68c9c007ee54e77ad0be04410de14
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-05-11 14:55:30 +0300

    Add Cinema_Room_Beijing.Designer.cs

--- Cinema_Room_Beijing.cs
+++ Cinema_Room_Beijing.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,6 +24,11 @@ namespace Cinema
             form1.Show();
             this.Hide();
         }
+
+        private void Online_info_button_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("This is Beijing Cinema Room.\nClick on the back button to return to Hallway.", "Online Help", MessageBoxButtons.OK);
+            //Online Helper MessageBox's message.
+        }
     }
 }
-
