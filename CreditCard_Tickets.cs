6894f81837db24b6e010b34569b6abb6efa775ec
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-05-21 23:33:49 +0300

    Add CreditCard_Tickets.cs

--- CreditCard_Tickets.cs
+++ CreditCard_Tickets.cs
@@ -8,3 +8,72 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+namespace Cinema
+{
+    public partial class CreditCard_Tickets : Form
+    {
+        public CreditCard_Tickets()
+        {
+            InitializeComponent();
+        }
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            var form1 = new Tickets();
+            form1.Closed += (s, args) => this.Close();
+            form1.Show();
+            this.Hide();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "" || textBox5.Text == "")
+            {
+                MessageBox.Show("Please fill all the fields first!");
+            }
+            else
+            {
+                var form1 = new Payment_Completed_Tickets();
+                form1.Closed += (s, args) => this.Close();
+                form1.Show();
+                this.Hide();
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers!");
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            }
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox3.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers!");
+                textBox3.Text = textBox3.Text.Remove(textBox3.Text.Length - 1);
+            }
+        }
+
+        private void TextBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox5.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers!");
+                textBox5.Text = textBox5.Text.Remove(textBox5.Text.Length - 1);
+            }
+        }
+
+        private void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox4.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers!");
+                textBox4.Text = textBox4.Text.Remove(textBox4.Text.Length - 1);
+            }
+        }
+    }
+}
+
